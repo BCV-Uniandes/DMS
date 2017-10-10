d419894620646a824a643bbe90861cc56f6c9510
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-10-10 11:13:32 -0500

    Dataloader: Cast mask as uint8

--- referit_loader.py
+++ referit_loader.py
@@ -203,6 +203,7 @@ class ReferDataset(data.Dataset):
             img = self.transform(img)
         if self.annotation_transform is not None:
             mask = mask.unsqueeze(0)
+            mask = mask.byte()
             mask = self.annotation_transform(mask)
         phrase = self.corpus.tokenize(phrase)
         return img, mask, phrase
