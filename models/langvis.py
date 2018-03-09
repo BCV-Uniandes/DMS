fdbce08d4a0694e736ffc06b8556fb09f92b22af
Juan Camilo Perez Santamaria <jc.perez13@uniandes.edu.co>
2018-03-09 12:05:50 -0500

    Added parameter 'concat_rt' to control if language representation is concatenated at each spatial location

--- models/langvis.py
+++ models/langvis.py
@@ -18,12 +18,13 @@ class LangVisNet(nn.Module):
                  vis_size=2688, num_filters=1, mixed_size=1000,
                  hid_mixed_size=1005, lang_layers=2, mixed_layers=3,
                  backend='dpn92', mix_we=False, lstm=False, pretrained=True,
-                 extra=True, gpu_pair=None, high_res=False, use_filters=False):
+                 extra=True, gpu_pair=None, high_res=False, concat_rt=False):
         super().__init__()
         self.high_res = high_res
         self.vis_size = vis_size
         self.num_filters = num_filters
         self.use_filters = use_filters
+        self.concat_rt = concat_rt
 
         if backend == 'dpn92':
             self.base = create_model(
@@ -42,13 +43,16 @@ class LangVisNet(nn.Module):
         lineal_in = hid_size + emb_size * int(mix_we)
 
 
-        if self.use_filters:
-            self.adaptative_filter = nn.Linear(
-                in_features=lineal_in, out_features=(num_filters * (vis_size + 8)))
-        else:
-            num_filters = 0 # So that comb_conv does not need more channels
 
-        self.comb_conv = nn.Conv2d(in_channels=(8 + emb_size + hid_size +
+        self.adaptative_filter = nn.Linear(
+                in_features=lineal_in, out_features=(num_filters * (vis_size + 8)))
+
+        if self.concat_rt:
+            rt_size = emb_size + hid_size
+        else:
+            rt_size = 0
+        
+        self.comb_conv = nn.Conv2d(in_channels=(8 + rt_size +
                                                 vis_size + num_filters),
                                    out_channels=mixed_size,
                                    kernel_size=1,
@@ -134,29 +138,28 @@ class LangVisNet(nn.Module):
         # Size: HxL?
         linear_in = linear_in.squeeze()
         # if self.mix_we:
-        if self.use_filters:
-            filters = self.adaptative_filter(linear_in)
-            # else:
-                # filters = self.adaptative_filter(lang)
-            filters = F.sigmoid(filters)
-            # LxFx(N+2)x1x1
-            filters = filters.view(
-                time_steps, self.num_filters, self.vis_size + 8, 1, 1)
-            p = []
-            for t in range(time_steps):
-                filter = filters[t]
-                p.append(F.conv2d(input=vis, weight=filter).unsqueeze(0))
+        filters = self.adaptative_filter(linear_in)
+        # else:
+            # filters = self.adaptative_filter(lang)
+        filters = F.sigmoid(filters)
+        # LxFx(N+2)x1x1
+        filters = filters.view(
+            time_steps, self.num_filters, self.vis_size + 8, 1, 1)
+        p = []
+        for t in range(time_steps):
+            filter = filters[t]
+            p.append(F.conv2d(input=vis, weight=filter).unsqueeze(0))
 
-            # LxFxH/32xW/32
-            p = torch.cat(p)
+        # LxFxH/32xW/32
+        p = torch.cat(p)
 
         # Lx(N + 2)xH/32xW/32
         vis = vis.unsqueeze(0).expand(time_steps, *vis.size())
         # Lx(N + F + H + E + 2)xH/32xW/32
-        if self.use_filters:
+        if self.concat_rt:
             q = torch.cat([vis, lang_mix, p], dim=2)
-        else: # There's no variable 'p'
-            q = torch.cat([vis, lang_mix], dim=2)
+        else: # Do not use lang_mix
+            q = torch.cat([vis, p], dim=2)
         # Lx1xSxH/32xW/32
         # print(mixed.size())
 
