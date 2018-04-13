8c264036cf2dae94111fc99e46e0c390d0bfc435
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-04-13 00:35:45 -0500

    Debug!

--- train.py
+++ train.py
@@ -54,7 +54,9 @@ def gather_monkeypatch(outputs, target_device, dim=0):
                               for k in out))
         if isinstance(out, list):
             print([x[0].size() for x in outputs])
-            return type(out)((gather_map(v[0]) for v in outputs))
+            ret = type(out)((gather_map(v[0]) for v in outputs))
+            print([x.size() for x in ret])
+            return ret
         return type(out)(map(gather_map, zip(*outputs)))
 
     # Recursive function calls like this create reference cycles.
