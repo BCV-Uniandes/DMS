110d6750d2e93643660b82956a9efcda09c836bf
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-05-07 10:47:02 -0500

    Disable spawning

--- train.py
+++ train.py
@@ -9,10 +9,10 @@ import os
 import time
 import argparse
 import os.path as osp
-import multiprocessing
+# import multiprocessing
 from urllib.parse import urlparse
 
-multiprocessing.set_start_method("spawn", force=True)
+# multiprocessing.set_start_method("spawn", force=True)
 
 # PyTorch imports
 import torch
